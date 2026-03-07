0d21ca50d7020dcf2c39c908be6a9e8483a0954b
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2026-03-07 14:50:56 -0300

    Malloc (Estudar dps)

---
+++ Estrutura de Dados/Códigos/Malloc/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int *array = NULL;
+    int tamanho = 0;
+    int num;
+
+    printf("Digite numeros (-1 para parar):\n");
+
+    while(1){
+        scanf("%d", &num);
+
+        if(num == -1){
+            break;
+        }
+
+        tamanho++;
+
+        array = realloc(array, tamanho * sizeof(int));
+
+        if(array == NULL){
+            printf("Erro de memoria!\n");
+            return 1;
+        }
+
+        array[tamanho - 1] = num;
+    }
+
+    printf("\nNumeros digitados:\n");
+
+    for(int i = 0; i < tamanho; i++){
+        printf("%d ", array[i]);
+    }
+
+    printf("\n");
+
+    free(array);
+
+    return 0;
+}
