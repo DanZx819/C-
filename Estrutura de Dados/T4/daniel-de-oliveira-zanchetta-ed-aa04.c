ad0f19ede70284f67619bc3457acba3c555e8d66
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2026-03-09 10:27:18 -0300

    Create daniel-de-oliveira-zanchetta-ed-aa04.c

---
+++ Estrutura de Dados/T4/daniel-de-oliveira-zanchetta-ed-aa04.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define CAPACIDADE 5
+
+int ITEM_INDEX = 0;
+
+enum bool { FALSE, TRUE };
+typedef enum bool Boolean;
+typedef long long duploLong;
+
+struct produto {
+    duploLong codigo;
+    double preco;
+};
+
+typedef struct produto Produto;
+
+typedef struct estoque_item {
+    Produto produto;
+    int quantidade;
+    int minimo;
+} EstoqueItem;
+
+Boolean estaAcabando(EstoqueItem item) {
+    if (item.quantidade <= item.minimo) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+Boolean estoqueEstaVazio() {
+    return ITEM_INDEX == 0;
+}
+
+int random_int(int min, int max) {
+    return (rand() % ((max - min) + 1)) + min;
+}
+
+int main(int argc, char const *argv[]) {
+
+    srand(time(NULL));
+
+    EstoqueItem inventario[CAPACIDADE];
+
+    Produto prod1;
+    prod1.codigo = 1111111111;
+    prod1.preco = 10.01;
+
+    EstoqueItem item1;
+    item1.produto = prod1;
+    item1.quantidade = random_int(1, 10);
+    item1.minimo = random_int(1, 5);
+
+    inventario[ITEM_INDEX++] = item1;
+
+    Produto prod2;
+    prod2.codigo = 2222222222;
+    prod2.preco = 22.22;
+
+    Produto prod3 = {1234567890, 98.76};
+    EstoqueItem item3 = {prod3, 333, 333};
+
+    inventario[ITEM_INDEX++] = item3;
+
+    if (estoqueEstaVazio() == FALSE) {
+
+        for (int i = 0; i < ITEM_INDEX; i++) {
+
+            EstoqueItem item = inventario[i];
+
+            printf(
+                "\nProduto: %lld (R$ %.2f) Estoque: %3d.",
+                item.produto.codigo,
+                item.produto.preco,
+                item.quantidade
+            );
+
+            if (estaAcabando(item)) {
+                printf(" [ESTA ACABANDO]");
+            }
+        }
+
+        printf("\n\n");
+    }
+
+    return 0;
+}
